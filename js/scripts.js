3aec6dd5f8cfc4fcf834df2f3987dc57da63ea64
Epicodus <epicodus@Epicodus-iMac.local>
2016-08-22 16:39:09 -0400

    make else if condition match html value

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,7 @@
       var governmentStartup = $("input:radio[name=governmentStartup]:checked").val();
 
       if (design === 'yes') {
+        alert ('yes');
         if (frontBack === 'frontend' || frontBack === 'either'){
           if (mobile === 'yes' || mobile === 'no' || mobile ==='either') {
             if (webapp ==='yes' || webapp ==='either') {
@@ -23,6 +24,7 @@
         }
 
         else if (design === 'no') {
+          alert ('no');
             if (frontBack != 'frontend') {
               if (mobile === 'yes' || mobile === 'either') {
                  if (webapp ==='yes' || webapp ==='no') {
@@ -36,7 +38,8 @@
               }
             }
 //This does not work
-      if (design === 'either') {
+      else if (design === 'somewhat') {
+          alert ('somewhat');
           if (frontBack === 'frontend' || frontBack === 'backend' || frontBack === 'either') {
             if (mobile==='yes' || mobile === 'no'){
               if (webapp === 'yes' || webapp === 'no') {
@@ -50,44 +53,44 @@
             }
 
 //This doesn't seem to work
-          if (frontBack === 'frontend') {
-            if (mobile === 'yes' || mobile === 'either'){
-              if (webapp === 'yes' || webapp === 'either'){
-                if (governmentStartup === 'government' || governmentStartup === 'startup' || governmentStartup === 'either'){
-                  $("#survey").hide();
-                  $("#css").slideToggle();
-                  $("#result").text(name + " " + "you should consider taking the CSS track")
-                }
-              }
-            }
-          }
-//This seems to work:
-          else if (frontBack === 'backend') {
-            if (mobile ==='yes' || mobile === 'either'){
-              if (webapp === 'yes' || webapp === 'no' || webapp === 'either'){
-                if (governmentStartup === 'startup' || governmentStartup === 'either'){
-                   $("#survey").hide();
-                   $("#ruby").fadeToggle();
-                   $("#result").text(name + " " + "you should consider learning Ruby.")
-                 }
-               }
-             }
-           }
-
-
-          else {
-            if (frontBack==='backend' || frontBack==='either') {
-              if (mobile==='yes' || mobile==='no' || mobile === 'either') {
-                if (webapp==='either' || webapp==='no') {
-                  if (governmentStartup === 'government' || governmentStartup === 'either') {
-                    $("#result").text(name + " " + "you should consider learning Java")
-                    $("#survey").hide();
-                    $("#java").show();
-
-                   }
-                 }
-               }
-             }
-           }
+//           if (frontBack === 'frontend') {
+//             if (mobile === 'yes' || mobile === 'either'){
+//               if (webapp === 'yes' || webapp === 'either'){
+//                 if (governmentStartup === 'government' || governmentStartup === 'startup' || governmentStartup === 'either'){
+//                   $("#survey").hide();
+//                   $("#css").slideToggle();
+//                   $("#result").text(name + " " + "you should consider taking the CSS track")
+//                 }
+//               }
+//             }
+//           }
+// //This seems to work:
+//           else if (frontBack === 'backend') {
+//             if (mobile ==='yes' || mobile === 'either'){
+//               if (webapp === 'yes' || webapp === 'no' || webapp === 'either'){
+//                 if (governmentStartup === 'startup' || governmentStartup === 'either'){
+//                    $("#survey").hide();
+//                    $("#ruby").fadeToggle();
+//                    $("#result").text(name + " " + "you should consider learning Ruby.")
+//                  }
+//                }
+//              }
+//            }
+//
+//
+//           else {
+//             if (frontBack==='backend' || frontBack==='either') {
+//               if (mobile==='yes' || mobile==='no' || mobile === 'either') {
+//                 if (webapp==='either' || webapp==='no') {
+//                   if (governmentStartup === 'government' || governmentStartup === 'either') {
+//                     $("#result").text(name + " " + "you should consider learning Java")
+//                     $("#survey").hide();
+//                     $("#java").show();
+//
+//                    }
+//                  }
+//                }
+//              }
+//            }
        })
     });
